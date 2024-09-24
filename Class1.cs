298cdd6fe074545ed82ff827d6d19a1568d2f1d8
michael.wortel@chasacademy.se <michael.wortel@chasacademy.se>
2024-09-24 06:24:18 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+ public class Rectangle
+{
+    private int _width;
+	private int _height;
+
+	//default konstruktur
+	public Rectangle()
+	{
+
+	}
+   
+	
+	//class constructor, will be called automatically when you create an instance of a class.
+    public Rectangle(int height, int width)
+	{
+		
+		//this. kopplar (private) _width med width
+		//funkar också att bra skita i this. och bara ha _width=width; så när vi skapar en ny rectangle blir den inkommande height blir den privata _height
+
+		this._width = width;
+		this._height = height;
+	}
+
+	//kopplar ihop width och height att vi kommer åt dem även UTANFÖR klassen även om själva värderna är privata från början
+	public int Width
+	{
+		get { return _width; }
+		set { _width = value; }
+	}
+
+    public int Height //{ get; set; }
+    {
+        get { return _height; }
+        set { _height = value; }
+    }
+
+
+	//metod sida gånger höjd metoden måste vara public
+	//variablarna är ihopkopplade så du behöver inte lägga variablarna i () som i en vanlig metod utan använd variabelnamn
+    public int AreaCalc()
+	{
+		return _height * _width;
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+Rectangle Cube = new Rectangle(4,6);
+Rectangle Cube2 = new Rectangle();
+Console.WriteLine("brädden är " + Cube.Width + " höjden är " + Cube.Height);
+int answer = Cube.AreaCalc();
+Console.WriteLine(answer);
